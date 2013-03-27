7be1fc4507bb33c145b15c2233d8a312347f278c
Brian Berg <b_berg@dslextreme.com>
2013-03-27 13:04:09 -0700

    slight update
    Nothing much to see here.

--- WindowsApplication3/Teacher.vb
+++ WindowsApplication3/Teacher.vb
@@ -5,26 +5,14 @@
 
     End Sub
 
-    Public Sub New(ByVal code As String, ByVal name As String,
+    Public Sub New(ByVal code As Integer, ByVal name As String,
             ByVal department As String)
         Me.Code = code
         Me.Name = name
         Me.Department = department
     End Sub
 
-    Public Property Code() As String
-        Get
-            Return m_Code
-        End Get
-        Set(ByVal value As String)
-            If value.Length > 4 Then
-                Throw New ArgumentException(
-                    "Maximum length of Code is 4 characters.")
-            Else
-                m_Code = value
-            End If
-        End Set
-    End Property
+    Public Property Code() As Integer
 
     Public Property Name() As String
 
