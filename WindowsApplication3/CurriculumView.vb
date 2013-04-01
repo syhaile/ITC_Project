fb892c2e44a823e80e2c210544b0c14e79c1a826
Brian Berg <b_berg@dslextreme.com>
2013-04-01 10:31:31 -0700

    nothing to see here...
    yet.

---
+++ WindowsApplication3/CurriculumView.vb
@@ -0,0 +1,9 @@
+﻿Public Class CurriculumView
+    Dim MyLabels()
+Private Sub CurriculumView_Load( sender As Object,  e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub 
+
+End Class
