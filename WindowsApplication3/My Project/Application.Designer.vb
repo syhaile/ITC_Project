58882ac2a3c86cd9ea4ba7523b82ac994f4d4ccf
Kevin <kmampuero@csupomona.edu>
2013-04-03 15:36:48 -0700

    First commit.. Updated a rough student view
    If it breaks everything, roll this sucker back.

--- WindowsApplication3/My Project/Application.Designer.vb
+++ WindowsApplication3/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication3.Form1
+            Me.MainForm = Global.WindowsApplication3.StudentView
         End Sub
     End Class
 End Namespace
