c16d9dd87d94be7ad08885a2d32f51759db7b2bb
Kevin <ampuerok@gmail.com>
2013-04-07 14:30:58 -0700

    Add loop for student list
    Also added multiple attributes to list items -- id and name, name is
    display

--- WindowsApplication3/Form1.vb
+++ WindowsApplication3/Form1.vb
@@ -112,4 +112,8 @@
         txtArea.Text = msg
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
--- WindowsApplication3/StudentView.vb
+++ WindowsApplication3/StudentView.vb
@@ -1,4 +1,23 @@
 ﻿Public Class StudentView
 
+    Dim ctrlStudentDB As New StudentDatabase
 
+    Private Sub StudentView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim students As Collection = Controller.getStudentDB()
+        Dim studentList As ArrayList = New ArrayList()
+        Dim tempStudent As New Student
+
+        For Each tempStudent In students
+            studentList.Add(New Student() With {.ID = tempStudent.ID, .Name = tempStudent.Name})
+            lbxStudentList.DataSource = studentList
+            lbxStudentList.ValueMember = "ID"
+            lbxStudentList.DisplayMember = "Name"
+        Next
+
+    End Sub
+
+    Private Sub lbxStudentList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbxStudentList.SelectedIndexChanged
+
+    End Sub
 End Class
