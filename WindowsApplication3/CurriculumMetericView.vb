9c65fceccf053f803a2071c4433ec44ac240534a
Brian Berg <b_berg@dslextreme.com>
2013-04-07 10:00:25 -0700

    moar stuff

--- WindowsApplication3/CurriculumMetericView.vb
+++ WindowsApplication3/CurriculumMetericView.vb
@@ -1,24 +1,23 @@
 ﻿Public Class CurriculumMetericView
    
-    Dim curriculumQuarterAndDate As String = "Spring 2012"
-    Dim avgGPA As Integer = 2.4
+    Private m_graduatedStudents, m_droppedStudents, m_minTimeTaken, m_maxTimeTaken, m_minNumberUnits, m_maxNumberUnits As Integer
+    Private m_avgGPA, m_avgTimeTaken, m_avgNumberofUnitsRemaining, m_avgNubmerOfUnitsTaken As Double
+    Private curriculumQuarterAndDate As String = "Spring 2012"
     Dim node As TreeNode 
-    
+ 
     Private Sub CurriculumMetericView_Load( sender As Object,  e As EventArgs) Handles MyBase.Load
+            Dim avgGPA As Double = 2.6           
             lblMetricsTitle.Text = curriculumQuarterAndDate + " Metrics"
             lblCurriculumTitle.Text = curriculumQuarterAndDate + " Curriculum"
             Dim m_avgGPAString As String = avgGPA.ToString()
-            lblAvgGPA.Text = "Average GPA: " + avgGPA
+            lblAvgGPA.Text = "Average GPA: " + m_avgGPAString
         
             'Populating the treeview for curriculum 
              node = New TreeNode 
     End Sub
+
     Private Sub CurriculumMetricView_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
         CurriculumView.Show()
     End Sub
-
-Private Sub lblMetricsTitle_Click( sender As Object,  e As EventArgs) Handles lblMetricsTitle.Click
-
-End Sub
 
 End Class
