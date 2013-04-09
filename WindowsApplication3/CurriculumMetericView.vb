f88a62293df62a40507ace52591a0cb2a85dd8b2
Brian Berg <dachsund@dslextreme.com>
2013-04-08 19:01:48 -0700

    update to curriculum metrics

--- WindowsApplication3/CurriculumMetericView.vb
+++ WindowsApplication3/CurriculumMetericView.vb
@@ -3,11 +3,8 @@
 Public Class CurriculumMetericView
 
     'in class variables for handling data generation 
-    Private m_graduatedStudents, m_droppedStudents, m_minTimeTaken, m_maxTimeTaken, m_minNumberUnits, m_maxNumberUnits,
-        m_avgTimeTaken, m_avgNumberofUnitsRemaining, m_avgNubmerOfUnitsTaken, m_curriculum As String
-
-
-
+    Private m_graduatedStudents As Integer
+    Private m_curriculum As String
     'For displaying class information in the class metrics view
     Dim classes As Collection = Controller.getCourseDB
     Dim classList As New List(Of Course)
@@ -22,7 +19,7 @@ Public Class CurriculumMetericView
 
     'For dispalying curriculum centric data
     Dim curriculums As Collection = Controller.getCurriculumDB
-    Dim curriclumList As New List(Of Curriculum)
+    Dim curriculumList As New List(Of Curriculum)
     Dim tempCurriculum As New Curriculum
 
     Public Sub CurriculumMetericView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -34,22 +31,50 @@ Public Class CurriculumMetericView
         'generate a list of curriculums
         For Each tempCurriculum In curriculums
             cmbxCurriclum.Items.Add(tempCurriculum.ID)
-            curriclumList.Add(tempCurriculum)
+            curriculumList.Add(tempCurriculum)
 
         Next
 
-        'generate a list of students 
-        For Each tempStudent In students
-            studentList.Add(tempStudent)
-            For Each Student In studentList
-                
-            Next
-        Next
+
 
 
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbxCurriclum.SelectedIndexChanged
+
+        titleChanges()
+        populateCurriculums()
+        calcedGraduated()
+
+    End Sub
+
+    Private Sub lbxCurriculum_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbxCurriculum.SelectedIndexChanged
+        ClassBox.Show()
+
+        ClassBox.Text = classList(lbxCurriculum.SelectedIndex).ID
+        lblUnit.Text = "Units: " + classList(lbxCurriculum.SelectedIndex).Units.ToString
+
+        'For each loop for displaying the prerequisite classes 
+        lblPrereq.Text = "Prerequite(s): "
+        For Each PreRequisitCourse As String In classList(lbxCurriculum.SelectedIndex).PreRequisitCourse()
+
+            lblPrereq.Text += PreRequisitCourse + " "
+        Next
+
+        lblCompanion.Text = "Companion Course(s): " + classList(lbxCurriculum.SelectedIndex).CompanionCourse
+
+
+    End Sub
+
+    Public Sub titleChanges()
+        'updating the titles 
+        m_curriculum = curriculumList(cmbxCurriclum.SelectedIndex).ID
+        lblMetricsTitle.Text = m_curriculum + " Metrics"
+        lblCurriculumTitle.Text = m_curriculum + " Curriculum"
+
+    End Sub
+
+    Public Sub populateCurriculums()
         lbxCurriculum.Items.Clear()
 
         For Each cid As String In tempCurriculum.ElectiveCourses.Courses
@@ -72,23 +97,53 @@ Public Class CurriculumMetericView
 
     End Sub
 
-    Private Sub lbxCurriculum_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbxCurriculum.SelectedIndexChanged
-        ClassBox.Show()
+    'For calculating graduated amount 
+    Public Sub calcedGraduated()
+        For Each classCount In students
+            Dim graduationCounter As Integer
+            classCount = tempStudent.Courses.Count
+            If classCount >= 59 Then
+                graduationCounter += 1
+                m_graduatedStudents = graduationCounter
+            Else
 
-        ClassBox.Text = classList(lbxCurriculum.SelectedIndex).ID
-        lblUnit.Text = "Units: " + classList(lbxCurriculum.SelectedIndex).Units.ToString
-
-        'For each loop for displaying the prerequisite classes 
-        lblPrereq.Text = "Prerequite(s): "
-        For Each PreRequisitCourse As String In classList(lbxCurriculum.SelectedIndex).PreRequisitCourse()
-
-            lblPrereq.Text += PreRequisitCourse + " "
+            End If
         Next
+        Dim grad As String
+        grad = m_graduatedStudents.ToString
+        lblGrad.Text = "Graduated Students: " + grad
+    End Sub
 
-        lblCompanion.Text = "Companion Course(s): " + classList(lbxCurriculum.SelectedIndex).CompanionCourse
-
+    Public Sub calcedDropped()
 
     End Sub
 
+    Public Sub avgQuarterRemaining()
+
+    End Sub
+
+    Public Sub avgTime()
+
+    End Sub
+
+    Public Sub minTime()
+
+    End Sub
+
+    Public Sub maxTime()
+
+    End Sub
+
+    Public Sub avgUnits()
+
+    End Sub
+
+    Public Sub minUnits()
+
+    End Sub
+
+    Public Sub maxUnits()
+
+    End Sub
 
 End Class
